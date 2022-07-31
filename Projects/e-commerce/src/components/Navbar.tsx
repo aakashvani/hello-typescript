b3b91111e453d90937e202bf2b624c8601b23385
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-07-31 23:39:56 +0530

    Navbar comppleted so far, added link, cart icon

--- Projects/e-commerce/src/components/Navbar.tsx
+++ Projects/e-commerce/src/components/Navbar.tsx
@@ -1,9 +1,46 @@
-import { Container, Navbar as NavbarBs } from "react-bootstrap";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { FaCartArrowDown } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
-      <Container>Navbar</Container>
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <FaCartArrowDown style={{ width: "1.4rem", height: "1.8rem" }} />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: "0",
+              right: "0",
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
     </NavbarBs>
   );
 };
