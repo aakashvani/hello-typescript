64a45002b45e5754eacbc661fb48c66e4ccdf228
Aakash Kumar <95979529+Aakashvani@users.noreply.github.com>
2022-07-31 22:28:58 +0530

    Build folder struture and navbar

--- Projects/e-commerce/src/App.tsx
+++ Projects/e-commerce/src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>hello ts</h1>
-  )
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
